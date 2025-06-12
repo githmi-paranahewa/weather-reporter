e3d866cf43eee1ab2e3aad5a80a342934b9529d0
Githmi Paranahewa <githmiph.20@itfac.mrt.ac.lk>
2025-06-12 21:10:35 +0530

    Do some minor changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const apiUrl = import.meta.env.VITE_WEATHER_API_URL;
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 export async function fetchWeatherData(city) {
-  const url = `${apiUrl}?key=${apiKey}&q=${city}&days=7`;
+  const url = `${apiUrl}?key=${apiKey}&q=${city}&days=8`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`API error: ${response.statusText}`);
